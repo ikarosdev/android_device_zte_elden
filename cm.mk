4d86f6bc42534b54ac0beb33fb20f0e8767a7f83
ikarosdev@gmail.com <ikarosdev@gmail.com>
2013-05-21 20:51:11 -0500

    Rename prebuilts folder, bring typical makefiles up to speed

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-## Specify phone tech before including full_phone
+# Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/cdma.mk)
 
 # Release name
@@ -16,3 +16,8 @@ PRODUCT_NAME := cm_elden
 PRODUCT_BRAND := zte
 PRODUCT_MODEL := elden
 PRODUCT_MANUFACTURER := zte
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="ZTE/N9120_METROPCS_USA/elden:4.0.4/IMM76I/20121119.142646.21040:user/release-keys" \
+    PRIVATE_BUILD_DESC="N9120_METROPCS_USA-user 4.0.4 IMM76I 20121119.142646.21040 release-keys"
